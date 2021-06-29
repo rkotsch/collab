f5254d63a9b364b426f1de1eff8e46650bfc7481
flbach <86286857+flbach@users.noreply.github.com>
2021-06-29 14:24:30 +0200

    Corrected and extended to three numbers.

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 
+# extend both to three numbers
 
-def multiply(number1, number2):
-    product = number1 * number2
+def multiply(number1, number2, number3):
+    product = number1 * number2 * number3
     return product
+print(str('The number is: ') + str(multiply(1,2,3)))
 
-print(str('The number is: ') + str(multiply(1,2)))
-
-def add(number1, number2):
-    add = number1 * number2
+def add(number1, number2, number3):
+    add = number1 + number2 + number3
     return add
+print(str('The number is: ') + str(add(1,2,3)))
 
-print(str('The number is: ') + str(add(1,2)))
-
+print('Extended, boss.')
