9336b445df25504c2315bcfb2535a575ec02ef59
Zita Mayer <zitamayer@protonmail.com>
2021-06-29 14:26:57 +0200

    corrected and extended file

--- app.py
+++ app.py
@@ -7,8 +7,13 @@ def multiply(number1, number2):
 print(str('The number is: ') + str(multiply(1,2)))
 
 def add(number1, number2):
-    add = number1 * number2
+    add = number1 + number2
     return add
 
 print(str('The number is: ') + str(add(1,2)))
 
+def substract(number1, number2):
+    substract = number1 - number2
+    return substract
+
+print(str('The number is: ') + str(substract(1,2)))
