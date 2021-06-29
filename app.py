d80e76e040408a1cb6dd5c90a23724424e16a596
Zita Mayer <zitamayer@protonmail.com>
2021-06-29 14:20:12 +0200

    more changes to app.py

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
+
+
 def multiply(number1, number2):
     product = number1 * number2
     return product
+
 print(str('The number is: ') + str(multiply(1,2)))
+
+def add(number1, number2):
+    add = number1 * number2
+    return add
+
+print(str('The number is: ') + str(add(1,2)))
+
